8d3473d3b542799b50256de25603e7ac4ffebfff
sangar-happy <sangar-happy@github.com>
2019-05-25 00:05:08 +0530

    Updated the icons in the toolbar.

--- app/src/main/java/com/example/internshiptest/MainActivity.java
+++ app/src/main/java/com/example/internshiptest/MainActivity.java
@@ -90,7 +90,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
         return true;
     }
 
@@ -102,7 +102,11 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.trophy) {
+            return true;
+        }
+
+        if (id == R.id.wallet) {
             return true;
         }
 
